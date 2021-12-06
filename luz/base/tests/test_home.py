c8a0ed7cb25c7a48653bd7f077d1a9926799b9b4
Henrique de Andrade <37959973+HenriqueCCdA@users.noreply.github.com>
2021-12-05 23:15:04 -0300

    Configuração inicial.

---
+++ luz/base/tests/test_home.py
@@ -0,0 +1,9 @@
+from http import HTTPStatus
+
+from django.test import Client
+
+
+def test_home_status_code(client: Client):
+    resp = client.get('/')
+
+    assert resp.status_code == HTTPStatus.OK
