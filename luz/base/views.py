ed60091e55b49b8a1e96100f1861d28d28adc860
Henrique de Andrade <37959973+HenriqueCCdA@users.noreply.github.com>
2021-12-06 02:36:56 -0300

    Criar a pagina do gráfico.
    close #3

--- luz/base/views.py
+++ luz/base/views.py
@@ -1,7 +1,16 @@
-# from django.shortcuts import render
-from django.shortcuts import HttpResponse
+from datetime import date
+
+from django.shortcuts import render
+
+
+data = {'date': [date(2020, 1, 1), date(2020, 2, 1), date(2020, 3, 1), date(2020, 4, 1), date(2020, 5, 1)],
+        'MW': [300, 455, 456, 80, 608]
+        }
 
 
 # Create your views here.
 def home(request):
-    return HttpResponse('<h1>Pagina da home</h1>')
+    ctx = {'date': [date.strftime("%d/%m/%Y") for date in data['date']],
+           'MW':  data['MW']
+           }
+    return render(request, "base/grafico.html", context=ctx)
