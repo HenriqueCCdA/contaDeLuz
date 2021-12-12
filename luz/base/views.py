1a9e9541f3c264020d7a32409b5ddf89702e471e
Henrique de Andrade <37959973+HenriqueCCdA@users.noreply.github.com>
2021-12-12 02:33:14 -0300

    Tirar os graficos da home
    close #12

--- luz/base/views.py
+++ luz/base/views.py
@@ -1,16 +1,5 @@
-from datetime import date
-
 from django.shortcuts import render
 
 
-data = {'date': [date(2020, 1, 1), date(2020, 2, 1), date(2020, 3, 1), date(2020, 4, 1), date(2020, 5, 1)],
-        'MW': [300, 455, 456, 80, 608]
-        }
-
-
-# Create your views here.
 def home(request):
-    ctx = {'date': [date.strftime("%d/%m/%Y") for date in data['date']],
-           'MW':  data['MW']
-           }
-    return render(request, "base/grafico.html", context=ctx)
+    return render(request, "base/home.html")
