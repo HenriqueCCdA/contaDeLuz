775136ed644eb9e47a8d30edea1b6c87b30fbb6e
Henrique de Andrade <37959973+HenriqueCCdA@users.noreply.github.com>
2021-12-13 00:11:53 -0300

    Criar testes para testar a navbar.
    close #18

--- luz/base/tests/test_home.py
+++ luz/base/tests/test_home.py
@@ -1,9 +1,19 @@
+import pytest
 from http import HTTPStatus
 
 from django.test import Client
 
+from luz.base.django_assertions import assertion_contains
 
-def test_home_status_code(client: Client):
-    resp = client.get('/')
 
-    assert resp.status_code == HTTPStatus.OK
+@pytest.fixture
+def response(client: Client):
+    return client.get('/')
+
+
+def test_home_status_code(response):
+    assert response.status_code == HTTPStatus.OK
+
+
+def test_home_navbar(response):
+    assertion_contains(response, '<a class="nav-link active" href="/"> Home </a>')
