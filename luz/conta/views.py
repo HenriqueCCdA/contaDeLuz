c1cc22aa33e3017b8b25262c3cce82944f211f0f
Henrique de Andrade <37959973+HenriqueCCdA@users.noreply.github.com>
2021-12-12 22:22:11 -0300

    Mudando um pouco o layout da pagina
    close #16

--- luz/conta/views.py
+++ luz/conta/views.py
@@ -7,9 +7,10 @@ def graph(request, slug):
 
     bills = list(Conta.objects.all().order_by('date'))
 
-    ctx = {'date': [bill.date.strftime("%d/%m/%Y") for bill in bills],
+    ctx = {'date': [bill.date.strftime("%m/%Y") for bill in bills],
            'amount_paid': [str(bill.amount_paid) for bill in bills],
-           'mw': [bill.mw for bill in bills]
+           'mw': [bill.mw for bill in bills],
+           'view_name': 'graph'
            }
 
     return render(request, f'{slug}/graph.html', context=ctx)
